eac023b733532f6522cd9add8c2338685ac60ca7
yvsn <yvaessen@hotmail.de>
2020-06-03 01:53:47 +0200

    formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,47 @@
 function background() {
 var earth = document.getElementById('untoggled');
     if (earth.style.display === 'none') {
-        earth.style.display = 'block'
+          earth.style.display = 'block'
     }
     else {
-        earth.style.display = 'none';
-    }
+          earth.style.display = 'none';
+     }
 }
 
 function date(){
-var currentDate = new Date();
-var month = currentDate.getMonth() +1;
-var day = currentDate.getDate();
-var year = currentDate. getFullYear();
-document.getElementById('currentdate').innerHTML = day + '.' + month + '.' + year;
+  var currentDate = new Date();
+    var month = currentDate.getMonth() +1;
+      var day = currentDate.getDate();
+        var year = currentDate. getFullYear();
+          document.getElementById('currentdate').innerHTML = day + '.' + month + '.' + year;
 }
 
 date();
 
 setInterval(clock, 1000);
 
-
 var secondHand = document.querySelector('[second-hand]')
-var minuteHand = document.querySelector('[minute-hand]')
-var hourHand = document.querySelector('[hour-hand]')
+  var minuteHand = document.querySelector('[minute-hand]')
+    var hourHand = document.querySelector('[hour-hand]')
 
 function clock(){
-var time = new Date()
-var second = time.getSeconds() / 60
-var minute = (second + time.getMinutes()) / 60
-var hour = (minute + time.getHours()) / 12
-setRotation(secondHand, second)
-setRotation(minuteHand, minute)
-setRotation(hourHand, hour)
+  var time = new Date()
+    var second = time.getSeconds() / 60
+      var minute = (second + time.getMinutes()) / 60
+        var hour = (minute + time.getHours()) / 12
+          setRotation(secondHand, second)
+            setRotation(minuteHand, minute)
+              setRotation(hourHand, hour)
 }
 
 function  setRotation(hands, rotationRatio) {
   hands.style.setProperty('--rotation', rotationRatio *360)
-  if(rotationRatio ===  0){
-  hands.style.background = "red";
+    if(rotationRatio ===  0){
+      hands.style.background = "red";
   }
   else{
-  hands.style.background = "black";
+      hands.style.background = "black";
   }
 }
+
 clock();
