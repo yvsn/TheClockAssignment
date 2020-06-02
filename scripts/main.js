3942576cf888d01fce33d8afddef65e5ecbe1718
yvsn <yvaessen@hotmail.de>
2020-06-02 11:33:48 +0200

    Change Background and Date

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,35 @@
+function background() {
+var earth = document.getElementById('untoggled');
+    if (earth.style.display === 'none') {
+        earth.style.display = 'block'
+    }
+    else {
+        earth.style.display = 'none';
+    }
+}
+
+function date(){
+var currentDate = new Date();
+var month = currentDate.getMonth() +1;
+var day = currentDate.getDate();
+var year = currentDate. getFullYear();
+document.getElementById('currentdate').innerHTML = day + '.' + month + '.' + year;
+}
+
+date();
+
 setInterval(clock, 1000);
 
-var secondHand= document.querySelector('[second-hand]')
-var minuteHand= document.querySelector('[minute-hand]')
-var hourHand= document.querySelector('[hour-hand]')
+
+var secondHand = document.querySelector('[second-hand]')
+var minuteHand = document.querySelector('[minute-hand]')
+var hourHand = document.querySelector('[hour-hand]')
 
 function clock(){
-var date = new Date()
-var second = date.getSeconds() / 60
-var minute = (second + date.getMinutes()) / 60
-var hour = (minute + date.getHours()) / 12
+var time = new Date()
+var second = time.getSeconds() / 60
+var minute = (second + time.getMinutes()) / 60
+var hour = (minute + time.getHours()) / 12
 setRotation(secondHand, second)
 setRotation(minuteHand, minute)
 setRotation(hourHand, hour)
@@ -16,6 +37,8 @@ setRotation(hourHand, hour)
 
 function  setRotation(hands, rotationRatio) {
   hands.style.setProperty('--rotation', rotationRatio *360)
-}
 
+}
 clock();
+
+secondHand.style.background = "#f3f3f3";
